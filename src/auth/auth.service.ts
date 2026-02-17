aa38166984f016f0a93ca6999d25e3685a2ed2ee
Cristina <cristina.soldan1@student.usv.ro>
2026-02-17 15:35:36 +0200

    add user login functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,12 @@ import { Injectable, BadRequestException } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import * as bcrypt from 'bcrypt'; 
 import { PrismaService } from 'prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
 
   async register(dto: RegisterDto) {
 
@@ -35,6 +37,37 @@ export class AuthService {
     return {
       message: 'Utilizator creat cu succes!',
       user: { email: newUser.email, fullName: newUser.fullName },
+    };
+  }
+ async login(dto: LoginDto) {
+
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+
+
+    if (!user) {
+      throw new BadRequestException('Invalid email or password');
+    }
+
+
+    const passwordMatches = await bcrypt.compare(dto.password, user.passwordHash);
+
+    if (!passwordMatches) {
+      throw new BadRequestException('Invalid email or password');
+    }
+
+  
+    const payload = { sub: user.id, email: user.email, role: user.role };
+    const token = await this.jwtService.signAsync(payload, {
+      secret: process.env.JWT_ACCESS_SECRET,
+      expiresIn: '1d',
+    });
+
+
+    return {
+      access_token: token,
+      email: user.email, 
     };
   }
 }
